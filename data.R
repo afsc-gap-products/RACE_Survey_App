ddba027715d5ea8349e565550e713652cc415622
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-05-05 09:41:02 -0700

    quick fixed bind_rows bug

--- data.R
+++ data.R
@@ -230,6 +230,7 @@ full_site <- dplyr::bind_rows(
     dplyr::filter(!(page0 == "collections" &
                       sub_page0 == "specific_collections")), 
   data_to_insert1) 
+full_site$order <- as.character(full_site$order)
 # need to think about how to remove srvy content we are not using (e.g., 2022 goa)
 
 
@@ -253,8 +254,8 @@ for (jj in 1:length(unique(full_site$page0))) {
         order = as.character(order),
         update_annually = as.logical(update_annually),
         across(dplyr::starts_with("srvy_"), as.logical),
-        in_survey_app = TRUE
-      )
+        in_survey_app = TRUE)
+    temp$lite_t_f <- as.logical(temp$lite_t_f)
     full_site <- dplyr::bind_rows(full_site, temp)
   }
 }
@@ -329,14 +330,12 @@ a <- paste0(
 "), ""),
   ifelse(comb0$sub_page0 == "", "      menu:
 ", ""),
-  collapse = ""
-)
+  collapse = "")
 
 site_yml <- gsub(
   pattern = "INSERT_NAVIGATION",
   replacement = a,
-  x = site_yml, fixed = TRUE
-)
+  x = site_yml, fixed = TRUE)
 
 # write new yml file
 utils::write.table(
