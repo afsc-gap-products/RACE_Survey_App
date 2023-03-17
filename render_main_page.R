78c396c274abffeaef9ac909b4074d0ee09803e3
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-03-16 22:34:54 -0700

    first init

---
+++ render_main_page.R
@@ -0,0 +1,41 @@
+## Render main page first
+# Write _site.yml
+
+site_yml <- base::readLines("templates/_site_template.txt")
+
+a <- ""
+for (ipage in unique(comb$page)) {
+  a <- paste(a, (paste0('   - text: "', ipage, '"
+    ', '  menu: 
+')))
+  
+  temp_subpages <- 
+    subset(x = comb, subset = page == ipage)
+  
+  for (isubpage in 1:nrow(temp_subpages)) {
+    a <- 
+      paste(a, (paste0('      - text: "', temp_subpages$sub_page[isubpage], '"
+    ', '     href: ', temp_subpages$web_page[isubpage], "
+")))
+  }
+}
+
+site_yml <- gsub(
+  pattern = "INSERT_NAVIGATION",
+  replacement = a,
+  x = site_yml, fixed = TRUE)
+
+# write new yml file
+utils::write.table(
+  x = site_yml,
+  file = "templates/_site.yml",
+  row.names = FALSE,
+  col.names = FALSE,
+  quote = FALSE
+)
+
+rm(a, ipage, isubpage, site_yml, temp_subpages)
+
+rmarkdown::render(input = "templates/index.Rmd",
+                  output_dir = "./docs/",
+                  output_file =  "index.html")
